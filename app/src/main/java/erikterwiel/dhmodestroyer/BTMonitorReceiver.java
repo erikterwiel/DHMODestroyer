e0e021d5b2b7b1168b4cd8946cf62adcc7b519e8
Erik Terwiel <erik.terwiel@gmail.com>
2017-10-23 02:27:42 -0400

    Added button to turn on Bluetooth from LaunchActivity.java

---
+++ app/src/main/java/erikterwiel/dhmodestroyer/BTMonitorReceiver.java
@@ -0,0 +1,39 @@
+package erikterwiel.dhmodestroyer;
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by Erik on 10/23/2017.
+ */
+
+public class BTMonitorReceiver extends BroadcastReceiver {
+
+    private final String TAG = "BTMonitorReceiver.java";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i(TAG, "onReceive() called");
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        String action = intent.getAction();
+        if (action.equals(bluetoothAdapter.ACTION_STATE_CHANGED)) {
+            int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, bluetoothAdapter.ERROR);
+            switch (state) {
+                case BluetoothAdapter.STATE_TURNING_OFF:
+                    Log.i(TAG, "Bluetooth is turning off");
+                    break;
+                case BluetoothAdapter.STATE_OFF:
+                    Log.i(TAG, "Bluetooth is now off");
+                    break;
+                case BluetoothAdapter.STATE_TURNING_ON:
+                    Log.i(TAG, "Bluetooth is turning on");
+                    break;
+                case BluetoothAdapter.STATE_ON:
+                    Log.i(TAG, "Bluetooth is now on");
+            }
+        }
+    }
+}
--- app/src/main/java/erikterwiel/dhmodestroyer/LaunchActivity.java
+++ app/src/main/java/erikterwiel/dhmodestroyer/LaunchActivity.java
@@ -1,13 +1,53 @@
 package erikterwiel.dhmodestroyer;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class LaunchActivity extends AppCompatActivity {
 
+    private final String TAG = "LaunchActivity.java";
+
+    BluetoothAdapter mBluetoothAdapter;
+    BTMonitorReceiver mBTMonitorReceiver;
+    Button mPairButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "onCreate() called");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
+
+        mPairButton = (Button) findViewById(R.id.launch_pair_button);
+
+        // Monitors bluetooth status
+        mBTMonitorReceiver = new BTMonitorReceiver();
+        IntentFilter btMonitorIntent =
+                new IntentFilter (BluetoothAdapter.ACTION_STATE_CHANGED);
+        registerReceiver(mBTMonitorReceiver, btMonitorIntent);
+
+        // Turns bluetooth on and displays paired devices on click
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        mPairButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!mBluetoothAdapter.isEnabled()) {
+                    Intent btEnableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivity(btEnableIntent);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        Log.i(TAG, "onStop() called");
+        super.onStop();
+        unregisterReceiver(mBTMonitorReceiver);
     }
 }
