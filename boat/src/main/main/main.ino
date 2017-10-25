670ec7f56ea08c92d0ca89ecdad529f775237b12
djoksimo <djoksimo@uwaterloo.ca>
2017-10-25 15:53:12 -0400

    added rudder function

--- boat/src/main/main/main.ino
+++ boat/src/main/main/main.ino
@@ -1,11 +1,14 @@
+#include <Servo.h>
+
 int motor1 = 13;
 int motor2 = 14;
 int vPower = 0;
 int hPower = 0;
-
+Servo rudder;
 
 void setup()
 {
+  rudder.attach(9);
   pinMode (motor1, OUTPUT);
   pinMode (motor2, OUTPUT);
   
@@ -31,14 +34,22 @@ analogWrite(motor2, vPower);
 */
 //forward thrust
 if (vPower > 50 && vPower <=100)
+{
 vPower -= 50;
 vPower *= 5;
-{
+
   digitalWrite(motor1, HIGH); //righthand propeller (clockwise)
   digitalWrite(motor2, LOW); //lefthand propeller (counter-clockwise)
   speed1 = map(vPower, 550, 1023, 0, 255);
   speed2 = map(vPower, 550, 1023, 0, 255);
 }
+  
+else if(hPower>=1 && hPower < 50)
+{
+  hPower = 180 - hPower; 
+  rudder.write(hPower);
+  delay(20);
+} 
 //reverse
 if(vPower < 50 && vPower >= 0)
 {
@@ -49,13 +60,19 @@ if(vPower < 50 && vPower >= 0)
   speed1 = map(vPower, 470, 0, 0, 255);
   speed2 = map(vPower, 470, 0, 0, 255);
 }
+
+else if (hPower >50 && hPower <= 100)
+{
+  hPower = 180 - hPower;
+  rudder.write(hPower);
+  delay(20);
+}
 else
 {
   speed1 = 0;
   speed2 = 0;
+  rudder.write(0);
 }
-  
-
 
 delay(60);
 }
