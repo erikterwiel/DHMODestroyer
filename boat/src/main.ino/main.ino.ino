4ca07deb450d8ec014bfa70ebe32704b53dc8491
djoksimo <djoksimo@uwaterloo.ca>
2017-10-25 14:56:46 -0400

    Serial control for thrust updated

---
+++ boat/src/main.ino/main.ino.ino
@@ -0,0 +1,68 @@
+int motor1 = 13;
+int motor2 = 14;
+int vPower = 0;
+int hPower = 0;
+
+
+void setup()
+{
+  pinMode (motor1, OUTPUT);
+  pinMode (motor2, OUTPUT);
+  
+  Serial.begin(9600);
+}
+void loop ()
+{
+
+if (Serial.available()>0)
+{
+  vPower = Serial.read();
+  hPower = Serial.read();
+}
+vPower = Serial.parseInt();
+hPower = Serial.parseInt();
+
+int speed1; //speed of righthand motor
+int speed2; //speed of lefthand motor
+
+/*
+analogWrite(motor1, hPower);
+analogWrite(motor2, vPower);
+*/
+//forward thrust
+if (vPower > 50 && vPower <=100)
+vPower -= 50;
+vPower *= 5;
+{
+  digitalWrite(motor1, HIGH); //righthand propeller (clockwise)
+  digitalWrite(motor2, LOW); //lefthand propeller (counter-clockwise)
+  speed1 = map(vPower, 550, 1023, 0, 255);
+  speed2 = map(vPower, 550, 1023, 0, 255);
+}
+//reverse
+if(vPower < 50 && vPower >= 0)
+{
+  vPower -= 50;
+  vPower *= 5;
+  digitalWrite(motor1, LOW); //righthand propeller (counter-clockwise)
+  digitalWrite(motor2, HIGH); //lefthand propeller (clockwise)
+  speed1 = map(vPower, 470, 0, 0, 255);
+  speed2 = map(vPower, 470, 0, 0, 255);
+}
+else
+{
+  speed1 = 0;
+  speed2 = 0;
+}
+  
+
+
+delay(60);
+}
+
+
+
+
+
+
+
