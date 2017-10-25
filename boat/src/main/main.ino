1c4f0ae1376bb7cf2b89bb93c197db4ea860495d
djoksimo <djoksimo@uwaterloo.ca>
2017-10-24 23:09:42 -0400

    bluetooth outline added to main

--- boat/src/main/main.ino
+++ boat/src/main/main.ino
@@ -1,9 +1,41 @@
-void setup() {
-  // put your setup code here, to run once:
+int led = 13;
+int state = 0;
+int flag = 0;
+int vPower = 0;
+int hPower = 0;
 
+void setup()
+{
+  pinMode(led, OUTPUT);
+  digitalWrite(led, LOW);
+  Serial.begin(9600);
+}
+void loop ()
+{
+if (Serial.available() > 0)
+{
+  state = Serial.read ();
+  flag = 0;
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+if (state == 0)
+{
+  digitalWrite(led, LOW);
+  if(flag == 0)
+  {
+    Serial.println("OFF");
+    flag = 1;
+  }
 }
+
+else if (state == 1)
+{
+  digitalWrite(led, HIGH);
+  if (flag == 0)
+  {
+    Serial.println("ON");
+    flag = 1;
+  }
+}
+}
+
