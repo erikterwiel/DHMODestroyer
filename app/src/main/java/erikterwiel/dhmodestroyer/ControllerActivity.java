ff90c5becd155674c4213f7fa7c2fdb299db9c60
ehterwie <ehterwie@uwaterloo.ca>
2017-10-24 23:12:14 -0400

    ControllerActivity now sends thrust and direction to Arduino

--- app/src/main/java/erikterwiel/dhmodestroyer/ControllerActivity.java
+++ app/src/main/java/erikterwiel/dhmodestroyer/ControllerActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.SeekBar;
 
 import java.io.IOException;
 import java.util.UUID;
@@ -20,26 +21,67 @@ public class ControllerActivity extends AppCompatActivity {
     private BluetoothDevice mDevice;
     private BluetoothAdapter mBluetoothAdapter;
     private BluetoothSocket mBluetoothSocket;
+    private SeekBar mVerticalStick;
+    private SeekBar mHorizontalStick;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         Log.i(TAG, "onCreate() called");
         super.onCreate(savedInstanceState);
+
+        View decorView = getWindow().getDecorView();
+        int uIOptions =   View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        decorView.setSystemUiVisibility(uIOptions);
         setContentView(R.layout.activity_controller);
 
         mDevice = getIntent().getParcelableExtra("Device");
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         new ConnectBluetooth().execute();
 
-        View decorView = getWindow().getDecorView();
-        int uIOptions =
-                  View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-        decorView.setSystemUiVisibility(uIOptions);
+        mVerticalStick = (SeekBar) findViewById(R.id.controller_vertical_stick);
+        mHorizontalStick = (SeekBar) findViewById(R.id.controller_horizontal_stick);
 
+        mVerticalStick.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                try {
+                    String toSend = "v" + Integer.toString(progress);
+                    Log.i(TAG, "Sending " + toSend + " to DHMO Destroyer");
+                    mBluetoothSocket.getOutputStream().write(toSend.getBytes());
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+
+        mHorizontalStick.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                try {
+                    String toSend = "h" + Integer.toString(progress);
+                    Log.i(TAG, "Sending " + toSend + " to DHMO Destroyer");
+                    mBluetoothSocket.getOutputStream().write(toSend.getBytes());
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
     }
 
     @Override
