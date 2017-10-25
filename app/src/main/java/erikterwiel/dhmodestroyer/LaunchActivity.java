27f84f83469192f01481ea79e9e27d3d485c853c
ehterwie <ehterwie@uwaterloo.ca>
2017-10-24 23:56:35 -0400

    ConnectActivity now launches only after Bluetooth is enabled

--- app/src/main/java/erikterwiel/dhmodestroyer/LaunchActivity.java
+++ app/src/main/java/erikterwiel/dhmodestroyer/LaunchActivity.java
@@ -1,8 +1,10 @@
 package erikterwiel.dhmodestroyer;
 
 import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -28,6 +30,11 @@ public class LaunchActivity extends AppCompatActivity {
         mBondButton = (Button) findViewById(R.id.launch_bond_button);
         mConnectButton = (Button) findViewById(R.id.launch_connect_button);
 
+        // Registers database to store which button is asking for Bluetooth access
+        SharedPreferences senderDatabase =
+                getSharedPreferences("senderDatabase", Context.MODE_PRIVATE);
+        final SharedPreferences.Editor databaseEditor = senderDatabase.edit();
+
         // Monitors Bluetooth state
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         mBTStateReceiver = new BTStateReceiver();
@@ -51,6 +58,8 @@ public class LaunchActivity extends AppCompatActivity {
                     startActivity(bondIntent);
                 } else {
                     Log.i(TAG, "Bluetooth not enabled, requesting on");
+                    databaseEditor.putString("toOpen", "Bond");
+                    databaseEditor.apply();
                     Intent btEnableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                     startActivity(btEnableIntent);
                 }
@@ -63,20 +72,24 @@ public class LaunchActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if (!mBluetoothAdapter.isEnabled()) {
                     Log.i(TAG, "Bluetooth not enabled, requesting on");
+                    databaseEditor.putString("toOpen", "Connect");
+                    databaseEditor.apply();
                     Intent btEnableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                     startActivity(btEnableIntent);
+                } else {
+                    Log.i(TAG, "Bluetooth enabled, launching ConnectActivity");
+                    Intent connectIntent = new Intent(LaunchActivity.this, ConnectActivity.class);
+                    startActivity(connectIntent);
                 }
-                Log.i(TAG, "Bluetooth enabled, launching ConnectActivity");
-                Intent connectIntent = new Intent(LaunchActivity.this, ConnectActivity.class);
-                startActivity(connectIntent);
+
             }
         });
     }
 
     @Override
-    protected void onStop() {
-        Log.i(TAG, "onStop() called");
-        super.onStop();
+    protected void onDestroy() {
+        Log.i(TAG, "onDestroy() called");
+        super.onDestroy();
         try {
             unregisterReceiver(mBTStateReceiver);
         } catch (IllegalArgumentException ex) {
