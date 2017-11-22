2799a6713ade390aa927b750f5968921525d2c3f
DJOKSIMO <djoksimo@uwaterloo.ca>
2017-11-22 13:48:36 -0500

    polished code

--- boat/src/main/main.ino
+++ boat/src/main/main.ino
@@ -5,13 +5,13 @@
 // Pin initialization
 int txd = 0;
 int rxd = 1;
-int enA = 5;
-int in1 = 6;
-int in2 = 7;
-int in3 = 8;
-int in4 = 9;
-int enB = 10;
-int srv = 11;
+int enA = 11;
+int in1 = 9;
+int in2 = 8;
+int in3 = 7;
+int in4 = 6;
+int enB = 5;
+int srv = 3;
 
 // Functionality variables
 char inputStream;
@@ -20,8 +20,6 @@ char inputChars[2];
 int inputInt;
 int inputIndex;
 int speedMotors;
-int motorSpeedRight; //speed of righthand motor (was 'speed1')
-int motorSpeedLeft; //speed of lefthand motor (was 'speed2')
 
 SoftwareSerial socket(txd, rxd);
 Servo servo;
@@ -36,14 +34,6 @@ void setup() {
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
   pinMode(enB, OUTPUT);
-
-  //Initial set-up motors
-  //Motor right forwards
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, HIGH);
-  //Motor left backwards
-  digitalWrite(in3, HIGH);
-  digitalWrite(in4, LOW);
 
   servo.attach(srv);
   servo.write(90);
@@ -114,46 +104,4 @@ void loop () {
   }
   delay(125);
 }
-
-/*
-
-
-
-  //forward thrust
-  if (vPower > 50 && vPower <= 100) {
-
-    vPower -= 50;
-    vPower *= 5;
-    digitalWrite(in1, HIGH);
-    digitalWrite(in2, LOW);
-    speed1 = map(vPower, 0, 1023, 0, 255);
-    speed2 = map(vPower, 0, 1023, 0, 255);
-
-  } else if (vPower < 50 && vPower >= 1) {
-
-    vPower -= 50;
-    vPower *= 5;
-    digitalWrite(in3, LOW);
-    digitalWrite(in4, HIGH);
-    speed1 = map(vPower, 0, 1023, 0, 255);
-    speed2 = map(vPower, 0, 1023, 0, 255);
-
-  } else {
-    speed1 = 0;
-    speed2 = 0;
-  }
-
-  if (hPower >= 1 && hPower < 50) {
-    hPower = 180 - hPower;
-    rudder.write(hPower);
-    delay(20);
-  } else if (hPower > 50 && hPower <= 100) {
-    hPower = 180 - hPower;
-    rudder.write(hPower);
-    delay(20);
-  } else {
-    rudder.write(0);
-  }
-*/
-
 
